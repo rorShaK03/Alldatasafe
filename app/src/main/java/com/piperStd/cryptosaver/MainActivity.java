777fb06d6c39db7390e43f01465829373259ee11
Shabad Kirill <shabad.k@yandex.ru>
2019-05-24 18:22:35 +0300

    The firs version of MainActivity interface

--- app/src/main/java/com/piperStd/cryptosaver/MainActivity.java
+++ app/src/main/java/com/piperStd/cryptosaver/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    CardView nfcCard;
+    CardView qrCard;
+    CardView serverCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        nfcCard = findViewById(R.id.card_nfc);
+        qrCard = findViewById(R.id.card_qr);
+        serverCard = findViewById(R.id.card_server);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        View.OnTouchListener cardTouchListener = new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View view, MotionEvent motion) {
+                if(motion.getAction() == MotionEvent.ACTION_DOWN)
+                    ((CardView)view).setCardElevation(4);
+                else if(motion.getAction() == MotionEvent.ACTION_UP)
+                    ((CardView)view).setCardElevation(6);
+                return true;
             }
-        });
+        };
+
+        nfcCard.setOnTouchListener(cardTouchListener);
+        qrCard.setOnTouchListener(cardTouchListener);
+        serverCard.setOnTouchListener(cardTouchListener);
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
