10629db64a4be479c85b54775b1dd28d0ccb76e2
Shabad Kirill <shabad.k@yandex.ru>
2019-05-27 01:06:15 +0300

    Interface of encryption/decryption

--- app/src/main/java/com/piperStd/cryptosaver/MainActivity.java
+++ app/src/main/java/com/piperStd/cryptosaver/MainActivity.java
@@ -1,15 +1,12 @@
 package com.piperStd.cryptosaver;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
